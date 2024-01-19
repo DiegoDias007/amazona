0cdf2203eeb8035fd5e418e40f8d9bc4b0b4cfb0
Diego <diegoddias007@gmail.com>
2024-01-19 08:44:24 -0300

    products service + controller

--- server/src/products/products.controller.ts
+++ server/src/products/products.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseUUIDPipe } from '@nestjs/common';
+import { ProductsService } from './products.service';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor(
+    private readonly productsService: ProductsService
+  ) {}
+
+  @Get()
+  async getAllProducts() {
+    return await this.productsService.getAllProducts()
+  }
+
+  @Get(":id")
+  async getSingleProduct(@Param("id", ParseUUIDPipe) id: string) {
+    return await this.productsService.getSingleProduct(id);
+  }
+}
--- server/src/products/products.service.ts
+++ server/src/products/products.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/database.service';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(
+    private readonly prisma: PrismaService
+  ) {}
+
+  async getAllProducts() {
+    const products = await this.prisma.product.findMany()
+    return products
+  }
+
+  async getSingleProduct(id: string) {
+    const product = await this.prisma.product.findUnique({where: { id }})
+    return product
+  }
+}
